ef63dc2a2f7080a0c0c855d6d86cceddd44151ca
Dragoș Niculescu <dragos.niculescu@cs.pub.ro>
2024-10-21 11:33:16 +0300

    pretty print

--- hello.asm
+++ hello.asm
@@ -2,14 +2,8 @@
 
 
 section .data
-    arr1 db 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x99, 0x88
-    len1 equ $-arr1
-    arr2 db 0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef
-    fmt db `arr1 = %d %x\n\0`
-    len2 equ $-arr2
-    val1 dd 0xabcdef01
-    val2 dd 0x62719012
-    twenty dd 0x1000  
+    arr1 db 0x11, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x99, 0x88
+    twenty dd 0x20  
  
 section .text
 global main
@@ -18,22 +12,23 @@ extern printf
 main:
     push rbp
     mov rbp, rsp
-    mov rdi, 0x123456789abcde00
+    nop
+    ; macro to print lower part of 64bit R/M
     mov rax, 0x123456789abcdef0
+    PRINTF64 `hhx=%3hhx hx=%6hx x=%11x lx=%21lx\n\x0` ,rax, rax, rax, rax 
+    PRINTF64 `hhd=%3hhd hd=%6hd d=%11d ld=%21ld\n\x0` ,rax, rax, rax, rax 
+    PRINTF64 `hhu=%3hhu hu=%6hu u=%11u lu=%21lu\n\x0` ,rax, rax, rax, rax 
     nop
-    PRINTF64 `rax=%llx\n\x0`, rax
-    xor esi, esi
-    mov sil, [arr2]  
-    imul esi, [twenty]  
-    mov ax, si
-    
-    nop
-    mov rsi, 0x123456789abcdef0
-    mov rax, rsi
-    PRINTF64 `hhx=%hhx hx=%hx x=%x lx=%lx\n\x0` ,rax, rax, rax, rax 
-    PRINTF64 `hhd=%hhd hd=%hd d=%d ld=%ld\n\x0` ,rax, rax, rax, rax 
-    PRINTF64 `hhu=%hhu hu=%hu u=%u lu=%lu\n\x0` ,rax, rax, rax, rax 
 
+gotcha:    
+    mov rsi, rax
+    xor esi, esi
+    PRINTF64 `rsi=%llx\n\x0`, rsi
+welcom_addition: 
+    mov sil, [arr1]  
+    PRINTF64 `esi=%x\n\x0`, rsi
+    imul esi, [twenty]  
+    PRINTF64 `esi=%x\n\x0`, rsi
 
     xor rax, rax
     leave
